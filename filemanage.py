d1ed498e2509fb78ef38aa667b7efa0d0a1f250c
Swartalf <ygavrilenko@gmail.com>
2015-02-18 13:52:58 +0200

    add export from svn, backup files

--- filemanage.py
+++ filemanage.py
@@ -4,20 +4,33 @@
 
 import os
 import conf
-from shutil import rmtree
+from shutil import rmtree, copy2
+from datetime import datetime
+from hashlib import md5
+import svn
+
 
 def delimiter():
-    if os.name=='nt':
+    if os.name == 'nt':
         return '\\'
     else:
         return '/'
 
-#return tuple of dict with setting
+
+def username():
+    if os.name == 'nt':
+        return os.environ['USERNAME']
+    else:
+        return os.environ['USER']
+
+
+# return tuple of dict with setting
 def set_samba_settings():
     main, s, samba = conf.load_config()
     return main,samba
 
-#check local directory and create if not exist
+
+# check local directory and create if not exist
 def check_local_storage():
     main,s = set_samba_settings()
     if not os.path.exists(main["local_storage"]):
@@ -31,10 +44,11 @@ def check_local_storage():
         if not os.path.isdir(main["log_dir"]):
             print "[WARN]\tPath "+str(main["log_dir"])+" not directory, please check it"
 
-#returm tuple(True/False, path)
+
+# return tuple(True/False, path)
 def check_ticket(ticket_id):
-    main,s = set_samba_settings()
-    #if directory with ticket number not exist return true
+    main, s = set_samba_settings()
+    # if directory with ticket number not exist return true
     if not os.path.exists(main["local_storage"]+delimiter()+str(ticket_id)):
         print "Ticket "+str(ticket_id)+" was not processed earlier"
         return True, main["local_storage"]+delimiter()+str(ticket_id)
@@ -45,23 +59,68 @@ def check_ticket(ticket_id):
             return False, None
         else:
             if len(os.listdir(main["local_storage"]+delimiter()+str(ticket_id)+delimiter())) < 3 or os.path.isfile(main["local_storage"]+delimiter()+str(ticket_id)+delimiter()+"notes.txt") is False:
-                print "[WARN]\tPath "+str(main["local_storage"])+delimiter()+str(ticket_id)+" already exist, but directory is empty or deploy is not complited.\n\t\tFlushing directory and reused it."
+                print "[WARN]\tPath "+str(main["local_storage"]) + \
+                      delimiter()+str(ticket_id) + \
+                      " already exist, but directory is empty or deploy is not complited." \
+                      "\n\t\tFlushing directory and reused it."
                 rmtree(main["local_storage"]+delimiter()+str(ticket_id))
                 return True, main["local_storage"]+delimiter()+str(ticket_id)
             with open(main["local_storage"]+delimiter()+str(ticket_id)+delimiter()+"notes.txt","r") as note:
-                print "Ticket#"+str(ticket_id)+" was processed earlier.\nNotes:"
+                print "Ticket#"+str(ticket_id)+" was processed earlier."
                 print note.read()
                 return False, None
 
-#create local structure and load file from svn
+
+# backuping prod file
+def get_production_backup(path, list_filename):
+    main, samba = set_samba_settings()
+    # connect temp disk B:
+    command = 'net use b: \\\\'+samba['smb_server']+"\\c$\MAXIMO /user:"+samba['smb_username']+' "'+samba['smb_password']+'"'
+    os.system(command)
+    file_info = {}  # dictionary with backup file  meta data
+    for file in list_filename:
+        if file[7:17] == 'MAXIMOTEST' and file[18:25] != 'SOURCES' and file[18:24] != 'DB_OBJ':
+            # create destination directory and backup files if it exists in source
+            if os.path.exists('b:'+delimiter()+file[18:].replace('/', '\\')):
+                if not os.path.isdir(path+file[:-len(os.path.basename(file))].replace('/', '\\')):
+                    os.makedirs(path+file[:-len(os.path.basename(file))].replace('/', '\\'))
+                copy2('b:'+delimiter()+file[18:].replace('/', '\\'), path+file[:-len(os.path.basename(file))].replace('/', '\\'))
+                file_info[file] = []
+                file_info[file].append(int(os.path.getsize(path+file[:-len(os.path.basename(file))].replace('/', '\\')+os.path.basename(file))))
+                file_info[file].append(md5(open(path+file[:-len(os.path.basename(file))].replace('/', '\\')+os.path.basename(file), 'rb').read()).hexdigest())
+    os.system('net use b: /delete')
+    return file_info
+
+
+# create local structure and load file from svn
 def prepare_file_for_deployment(ticket_id, svn_id):
     status, path = check_ticket(ticket_id)
-    #print status, path
     if status is True:
         os.mkdir(path)
         os.mkdir(path+delimiter()+"for_deployment")
         os.mkdir(path+delimiter()+"backup")
-        #print os.listdir(path)
+        with open(path+delimiter()+'notes.txt', 'w') as f:
+            f.write("Date:\t\t"+str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+"\n")
+            f.write("Ticket:\t\t"+str(svn_id)+"\n")
+            f.write("Deployer:\t"+str(username())+"\n")
+            f.write("-"*50+" File for deploy"+"-"*50+"\n")
+            f.write('%-70s\t%9s\t%32s\n%s\n' % ('Filename', 'Size(byte)', 'md5hash', '-'*116))
+            svn_file_info = svn.svn_export_files(svn_id, path+delimiter()+"for_deployment")
+            list_filename = sorted(svn_file_info.keys())
+            for filename in list_filename:
+                f.write('%-70s\t%9s\t%32s\n' % (filename, svn_file_info[filename][0], svn_file_info[filename][1]))
+            backup_file = get_production_backup(path+delimiter()+"backup", list_filename)
+            f.write("-"*50+" Backup  files "+"-"*50+"\n")
+            for filename in sorted(backup_file.keys()):
+                f.write('%-70s\t%9s\t%32s\n' % (filename, svn_file_info[filename][0], svn_file_info[filename][1]))
 
+    return 0
 
-prepare_file_for_deployment(1232312,2323)
+"""
+get_production_backup('d:\\tmp\\bpms\\', ['/trunk/MAXIMOTEST/custom/app/rtt/mbo/RTTDocsMbo.class',
+                                          '/trunk/MAXSAP/custom/app/rtt/mbo/RTTDocsMbo.class',
+                                          '/trunk/MAXIMOTEST/psdi/jsp/app/wotrack/ActionsWO.class',
+                                          '/trunk/MAXIMOTEST/SOURCES/custom/app/rtt/mbo/RTTDocsMbo.java',
+                                          '/trunk/DB_OBJ/VIEW.test.sql'])
+"""
+prepare_file_for_deployment(1632111, 5385)
--- svn.py
+++ svn.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#ygavrilenko@gmail.com
+# ygavrilenko@gmail.com
 
 import pysvn
 import conf
 import datetime
+import os
+from hashlib import md5
 
-#get settings from config.ini
+
+# get settings from config.ini
 def set_svn_settings():
-    m,svn, s = conf.load_config()
+    m, svn, s = conf.load_config()
     client = pysvn.Client()
     client.set_default_username(svn['svn_username'])
     client.set_default_password(svn['svn_password'])
@@ -16,41 +19,59 @@ def set_svn_settings():
     return client, url
 
 
-#return log for revision or range of revisions
+# return log for revision or range of revisions
 def svn_show_log(start_id, end_id):
     client, url = set_svn_settings()
-    result=""
-    for count in range(0,(end_id-start_id)+1):
-        revision_number=str(client.log(url,revision_start=pysvn.Revision( pysvn.opt_revision_kind.number,start_id ),
-                             revision_end=pysvn.Revision( pysvn.opt_revision_kind.number,end_id),
-                             discover_changed_paths=True,limit=0)[count].get("revision"))[22:-1]
-        revision_message=client.log(url,revision_start=pysvn.Revision( pysvn.opt_revision_kind.number,start_id ),
-                             revision_end=pysvn.Revision( pysvn.opt_revision_kind.number,end_id),
-                             discover_changed_paths=True,limit=0)[count].get("message")
-        revision_date=client.log(url,revision_start=pysvn.Revision( pysvn.opt_revision_kind.number,start_id ),
-                             revision_end=pysvn.Revision( pysvn.opt_revision_kind.number,end_id),
-                             discover_changed_paths=True,limit=0)[count].get("date")
-        revision_date=datetime.datetime.fromtimestamp(int(revision_date)).strftime('%Y-%m-%d %H:%M:%S')
-        revision_author=client.log(url,revision_start=pysvn.Revision( pysvn.opt_revision_kind.number,start_id ),
-                             revision_end=pysvn.Revision( pysvn.opt_revision_kind.number,end_id),
+    result = ""
+    for count in range(0, (end_id-start_id)+1):
+        revision_number = str(client.log(url, revision_start=pysvn.Revision( pysvn.opt_revision_kind.number, start_id),
+                             revision_end=pysvn.Revision(pysvn.opt_revision_kind.number ,end_id),
+                             discover_changed_paths=True, limit=0)[count].get("revision"))[22:-1]
+        revision_message = client.log(url, revision_start=pysvn.Revision(pysvn.opt_revision_kind.number, start_id),
+                             revision_end=pysvn.Revision(pysvn.opt_revision_kind.number, end_id),
+                             discover_changed_paths=True, limit=0)[count].get("message")
+        revision_date = client.log(url, revision_start=pysvn.Revision(pysvn.opt_revision_kind.number, start_id),
+                             revision_end=pysvn.Revision( pysvn.opt_revision_kind.number, end_id),
+                             discover_changed_paths=True, limit=0)[count].get("date")
+        revision_date = datetime.datetime.fromtimestamp(int(revision_date)).strftime('%Y-%m-%d %H:%M:%S')
+        revision_author = client.log(url,revision_start=pysvn.Revision(pysvn.opt_revision_kind.number, start_id),
+                             revision_end=pysvn.Revision(pysvn.opt_revision_kind.number, end_id),
                              discover_changed_paths=True,limit=0)[count].get("author")
-        qty_file=len(client.log(url,revision_start=pysvn.Revision( pysvn.opt_revision_kind.number,start_id ),
-                             revision_end=pysvn.Revision( pysvn.opt_revision_kind.number,end_id),
-                             discover_changed_paths=True,limit=0)[count].get("changed_paths"))
-        result+="#"*80+"\n  Rev:\t\t\t"+revision_number+"\n  Author:\t\t"+revision_author+"\n  Comments:\t\t"+revision_message+"\n  Commit date:\t"+revision_date+"\n  Total object:\t"+str(qty_file)+"\n"
-        for file in range(0,qty_file):
-            f_name=client.log(url,revision_start=pysvn.Revision( pysvn.opt_revision_kind.number,start_id ),
-                             revision_end=pysvn.Revision( pysvn.opt_revision_kind.number,end_id),
+        qty_file = len(client.log(url, revision_start=pysvn.Revision( pysvn.opt_revision_kind.number, start_id),
+                             revision_end=pysvn.Revision(pysvn.opt_revision_kind.number, end_id),
+                             discover_changed_paths=True, limit=0)[count].get("changed_paths"))
+        result += "-"*116+"\n  Rev:\t\t\t"+revision_number+"\n  Author:\t\t"+revision_author+"\n  Comments:\t\t"+revision_message+"\n  Commit date:\t"+revision_date+"\n  Total object:\t"+str(qty_file)+"\n"
+        for file in range(0, qty_file):
+            f_name = client.log(url, revision_start=pysvn.Revision( pysvn.opt_revision_kind.number, start_id),
+                             revision_end=pysvn.Revision(pysvn.opt_revision_kind.number,end_id),
                              discover_changed_paths=True,limit=0)[count].get("changed_paths")[file]['path']
-            f_status=client.log(url,revision_start=pysvn.Revision( pysvn.opt_revision_kind.number,start_id ),
-                             revision_end=pysvn.Revision( pysvn.opt_revision_kind.number,end_id),
+            f_status = client.log(url,revision_start=pysvn.Revision(pysvn.opt_revision_kind.number, start_id),
+                             revision_end=pysvn.Revision(pysvn.opt_revision_kind.number, end_id),
                              discover_changed_paths=True,limit=0)[count].get("changed_paths")[file]['action']
-            result+="\t"+f_status+" "+f_name+"\n"
+            result += "\t"+f_status+" "+f_name+"\n"
     return result
 
 
+# export file by svn and return dictionary[filename]= [hashsum, size]
+def svn_export_files(svn_id, path):
+    client, url = set_svn_settings()
+    qty_file = len(client.log(url, revision_start=pysvn.Revision(pysvn.opt_revision_kind.number, svn_id),
+                             revision_end=pysvn.Revision(pysvn.opt_revision_kind.number, svn_id),
+                             discover_changed_paths=True, limit=0)[0].get("changed_paths"))
+    raw_files = []
+    for file in range(0, qty_file):
+        raw_files.append(client.log(url, revision_start=pysvn.Revision(pysvn.opt_revision_kind.number, svn_id),
+                             revision_end=pysvn.Revision(pysvn.opt_revision_kind.number, svn_id),
+                             discover_changed_paths=True, limit=0)[0].get("changed_paths")[file]['path'])
+    dirs = sorted(set(os.path.dirname(dir) for dir in raw_files))  # set with only directory's
+    files = [i for i in raw_files if len(os.path.basename(i).split('.')) >= 2]  # list with only files
+    [os.makedirs(path+directory) for directory in dirs]  # create directory tree
+    file_info = {}  # dictionary with new file  meta data
+    for file in files:
+        client.export(url+file, path+file, peg_revision=pysvn.Revision(pysvn. opt_revision_kind.number, svn_id),
+                      force=True, recurse=False)
+        file_info[file] = []
+        file_info[file].append(int(os.path.getsize(path+file)))
+        file_info[file].append(md5(open(path+file, 'rb').read()).hexdigest())
 
-
-
-#a=svn_show_log(5323,5323)
-#print a
+    return file_info
