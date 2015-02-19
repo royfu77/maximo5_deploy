0de3e9d1ac8dc12f5649e61751d16ecb022e6931
Swartalf <ygavrilenko@gmail.com>
2015-02-19 11:37:38 +0200

    Add simple password encryption

---
+++ encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+#ygavrilenko@gmail.com
+# very simple linear encoding
+
+import sys
+
+
+def enc(password):
+    result = [chr(int(i, 16) ^ 0xb) for i in [hex(ord(i)) for i in password]]
+    print ''.join(result)
+    return 0
+
+
+enc(sys.argv[1])
